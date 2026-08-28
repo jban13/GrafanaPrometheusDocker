adc770c603fbfcdc1cfd0ef3ea4c69c831493c20
Jan <github@janborgmeier.com>
2026-08-28 19:49:43 +0200

    Update target file with current HOST_IP
    Replace DOCKER_HOST_IP in the target file with the current HOST_IP.

--- SetupMonitoring.sh
+++ SetupMonitoring.sh
@@ -50,10 +50,6 @@ copy_and_edit() {
     target_dir="$(dirname -- "$target_file")"
 
     HOST_IP=$(ip route get 1.1.1.1 | awk '{print $7; exit}')
-
-    sed -i
-        "s/DOCKER_HOST_IP/${HOST_IP}/g"
-        "$PROMETHEUS_CONFIG"
     
     [[ -f "$source_file" ]] || fail "Quelldatei fehlt: $source_file"
     mkdir -p -- "$target_dir"
@@ -69,6 +65,10 @@ copy_and_edit() {
         log "Konfiguration liegt bereits am Ziel: $target_file"
     fi
 
+    sed -i
+        "s/DOCKER_HOST_IP/${HOST_IP}/g"
+        "$target_file"
+        
     log "Oeffne $target_file zur Bearbeitung"
     "${EDITOR_CMD[@]}" "$target_file"
 }
