04912c4f63c3afd311ef2064c39ca5418e815caa
Jan <github@janborgmeier.com>
2026-08-28 18:58:51 +0200

    Remove read-only flag from volume mounts

--- SetupMonitoring.sh
+++ SetupMonitoring.sh
@@ -92,7 +92,7 @@ else
         --restart unless-stopped \
         -d \
         -p 0.0.0.0:9221:9221 \
-        -v "${PVE_CONFIG}:/etc/prometheus/pve.yml:ro" \
+        -v "${PVE_CONFIG}:/etc/prometheus/pve.yml" \
         prompve/prometheus-pve-exporter
 fi
 
@@ -114,7 +114,7 @@ else
         --restart unless-stopped \
         -p 9090:9090 \
         -d \
-        -v "${PROMETHEUS_CONFIG}:/etc/prometheus/prometheus.yml:ro" \
+        -v "${PROMETHEUS_CONFIG}:/etc/prometheus/prometheus.yml" \
         -v prometheus-data:/prometheus \
         prom/prometheus
 fi
